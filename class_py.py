9390d8253f2c9624f405db2fb03a18e6ce129bdd
alexewd <alexewd@gmail.com>
2020-02-01 19:37:54 +0300

    Ad files and updates

--- class_py.py
+++ class_py.py
@@ -1,18 +1,23 @@
 # First class
 class Goat():
-	name = ' '
-	age = 0
-	wight = 0.0
 
-	def show(self):
-		print(self.name)
-		print(self.age)
-		print(self.wight)
+    def __init__(self, name, age, wight):
 
-goat = Goat()
+        self.name = name
+        self.age = age
+        self.wight = wight
 
-goat.name = 'Julia'
-goat.age = 2
-goat.wight = 2.0
+    def show(self):
+
+        print(self.name)
+        print(self.age)
+        print(self.wight)
+
+
+goat = Goat('Irisha',2,3.0)
+
+# goat.name = 'Julia'
+# goat.age = 2
+# goat.wight = 2.0
 
 goat.show()
